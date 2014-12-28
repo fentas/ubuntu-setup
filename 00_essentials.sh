d0d227110863d82147301dec9171d8c12c1829b7
user <jan.guth@gmail.com>
2014-12-28 20:07:48 +0100

    refine

--- 00_essentials.sh
+++ 00_essentials.sh
@@ -5,6 +5,9 @@ if [ ! $SUDO_USER ]; then
 	exit 1
 fi
 
+# firefox developer daily (aurora)
+sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora
+
 apt-get update && apt-get install -y \
 	build-essential checkinstall \
 	guake zsh \
@@ -12,7 +15,8 @@ apt-get update && apt-get install -y \
 	texlive texlive-lang-german texlive-doc-de texlive-latex-extra latexmk \
 	pidgin pidgin-otr pidgin-skype \
 	git subversion \
-	curl unzip imagemagick page-crunch vlc gimp
+	curl unzip imagemagick page-crunch vlc gimp \
+  firefox
 
 # page-crunch
 # - GUI/frontend to psutils programs, like psnup, psbook
