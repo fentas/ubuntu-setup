c596753a98a820cea5858d399e949cb3c5fb8fac
fentas <jan.guth@gmail.com>
2015-08-26 16:54:53 +0200

    vim update

--- 02_vim.sh
+++ 02_vim.sh
@@ -14,7 +14,7 @@ VERSION="7.4"
 
 if [ $? -ne 0 ] || [ -z "$(vim -h | grep servername)" ]; then
 	wget ftp://ftp.vim.org/pub/vim/unix/vim-$VERSION.tar.bz2 -O /tmp/vim-release.tar.bz2 \
-		&& cd /tmp/ && tar -xvf ./vim-release.tar.bz2 && cd vim-*/
+		&& cd /tmp/ && tar -xvf ./vim-release.tar.bz2 && cd vim*/
 
 	if [ $? -eq 0 ]; then
 		apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev \
